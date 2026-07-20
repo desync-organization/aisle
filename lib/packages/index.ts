f8585a603800ca66c79750d14f893a3aca40592c
Krishang-Zinzuwadia <krishangzinzuwadia@gmail.com>
2026-07-21 03:33:53 +0530

    feat: curate public launch package blueprints (#8)

---
+++ lib/packages/index.ts
@@ -0,0 +1,20 @@
+export {
+  PACKAGE_BLUEPRINT_SCHEMA_VERSION,
+  createUnresolvedLocatorPlan,
+  githubSkillLocatorKey,
+  githubSkillLocatorSchema,
+  launchLicenseSpdxSchema,
+  licenseEvidenceClassSchema,
+  packageBlueprintMemberSchema,
+  packageBlueprintSchema,
+  packageCategories,
+  packageColorTokens,
+  packageIconTokens,
+  parsePackageBlueprint,
+  type GitHubSkillLocator,
+  type PackageBlueprint,
+  type PackageBlueprintMember,
+  type UnresolvedPackageLocatorPlan,
+} from "./package-blueprint";
+
+export { getLaunchPackageBlueprint, launchPackageBlueprints } from "./launch-blueprints";
